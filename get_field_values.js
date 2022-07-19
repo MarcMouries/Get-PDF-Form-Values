3533ab64b79f9114b0be5da5249bd7cfc3997abc
MarcMouries <marc@mouries.net>
2022-07-19 09:49:49 -0400

    print type list in table format

--- get_field_values.js
+++ get_field_values.js
@@ -32,15 +32,22 @@ const getPDF_Form = async (file_path) => {
 
 const list_PDF_Form_Fields = async (file_path) => {
     const form = await getPDF_Form(file_path);
-    const fields = form.getFields()
-    console.log("Field Name           | Field Type");
-    console.log("---------------------------------");
+    const fields = form.getFields();
+    console.log("╔════════════════════════════════╦════════════════╦════════════════════╗")
+    console.log("║ Field Name                     ║  Field Type    ║  Notes             ║");
+    console.log("╠════════════════════════════════╬════════════════╬════════════════════╣")
     fields.forEach(field => {
         const field_name = field.getName()
-        const type = field.constructor.name
-        const to_print = field_name.padEnd(20, ' ') + " | " + type.substring(3);
-        console.log(to_print)
+        const field_type = field.constructor.name;
+        let to_print = "║ " + field_name.padEnd(30, ' ') + " ║ " + field_type.substring(3).padEnd(15, ' ') + "║ ";
+
+        if (field_type == 'PDFDropdown') {
+            const selections = field.getSelected()
+            to_print  += 'Multiselect:' + field.isMultiselect();
+        }
+        console.log(to_print.padEnd(71, ' ') + "║ ")
     })
+    console.log("╚════════════════════════════════╩════════════════╩════════════════════╝");
 }
 
 
@@ -53,7 +60,6 @@ const list_PDF_Form_Values = async (file_path) => {
     fields.forEach(field => {
         const field_name = field.getName()
         const type = field.constructor.name
-        // console.log('Field type = ', type);
 
         if (type == 'PDFTextField') {
             result[field_name] = field.getText();
@@ -62,7 +68,14 @@ const list_PDF_Form_Values = async (file_path) => {
             result[field_name] = field.isChecked();
         }
         else if (type == 'PDFDropdown') {
-            result[field_name] = field.getSelected();
+            const selections = field.getSelected();
+            if (! field.isMultiselect()) {
+                let selected_value = selections[0];
+                result[field_name] = selected_value;
+            }
+            else {
+                result[field_name] = field.getSelected();
+            }
         }
         else if (type == 'PDFRadioGroup') {
             result[field_name] = field.getSelected();
